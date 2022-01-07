b3f71ed911d8d896ce93d4f7871285647be1a12a
Hendrik Brouwer <h.brouwer@gmail.com>
2022-01-07 19:44:10 +0100

    halfway through the lesson about adding data ROOM Database #2  Insert Data 6.08

---
+++ app/src/main/java/com/mosamesadev/roomdatabasetutorial/fragments/list/ListFragment.kt
@@ -0,0 +1,29 @@
+package com.mosamesadev.roomdatabasetutorial.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mosamesadev.roomdatabasetutorial.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class listFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
+    }
+
+
+}
